5a66c2847af68cb7837f1acf3a403d3703d12a4d
okabasele <ocekabasele@gmail.com>
2022-01-13 15:32:35 +0100

    check erreur login, manque erreur register (verif password et email)

--- auth/register.php
+++ auth/register.php
@@ -2,6 +2,7 @@
 require './class/database-connection.php';
 require './class/util.php';
 require './class/controller.php';
+require './class/user.php';
 
 //Recuperer la connection à la bdd
 $dbconnect = Util::getDatabaseConnection();
@@ -22,7 +23,9 @@ if (isset($_POST['register'])) {
 if (isset($_POST['login'])) {
 	if (!empty($_POST['username']) && !empty($_POST['password'])) {
 		if ($_POST['login'] === "send") {
-			$isValid = login($connect, $_POST['username'], $_POST['password']);
+			// $isValid = login($connect, $_POST['username'], $_POST['password']);
+			$isValid = User::isUserInDatabase($connect, $_POST['username'], $_POST['password']);
+			
 		}
 	}
 }
@@ -71,22 +74,51 @@ function checkPassword(mysqli $connect, string $username, string $passwordToChec
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
-
+<style>
+	input.inputError{
+    box-shadow: 0 0px 6px 0 #FB3640;
+    border-color:#FB3640 ;
+    background-image: url("https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061132_960_720.png");
+    background-size: 20px;
+    background-position: 250px 10px;
+    background-repeat: no-repeat;
+}
+</style>
 <body>
 	<form method="POST">
 		<label for="">register</label>
 		<input name="username" placeholder="USERNAME" type="text">
 		<input name="email" placeholder="EMAIL" type="text">
 		<input name="password" placeholder="PASSWORD" type="password">
-		<!-- <input name="passwordConfirm" placeholder="PASSWORD" type="password"> -->
+		<input name="passwordConfirm" placeholder="PASSWORD" type="password">
 		<input type="submit" name="register" value="send">
 	</form>
-	<form method="POST">
+	<form id="logForm" method="POST">
 		<label for="">login</label>
-		<input name="username" placeholder="USERNAME" type="text">
-		<input name="password" placeholder="PASSWORD" type="password">
+		<input id="logUid" name="username" placeholder="USERNAME" type="text">
+		<input id="logPwd" name="password" placeholder="PASSWORD" type="password">
 		<input type="submit" name="login" value="send">
 	</form>
 </body>
-
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<?php
+switch ($isValid) {
+	case -1:
+		echo '<script>swal("Authentification failed", "The user is not found.", "error");
+		logUid.classList.add("inputError");
+		logPwd.classList.remove("inputError");</script>';
+		break;
+	case -2:
+		echo '<script>
+		var logUid = document.getElementById("logUid");
+		var logPwd = document.getElementById("logPwd");
+		swal("Authentification failed", "Your password is incorrect.", "error");
+		logPwd.classList.add("inputError");
+		logUid.classList.remove("inputError"); </script>';
+		break;
+	case 1:
+	echo '<script>console.log("Auth ok")</script>';
+		break;
+}
+?>
 </html>
--- class/user.php
+++ class/user.php
@@ -1,40 +1,76 @@
 <?php
 // Classe qui contient des fonctions statiques qui s'occupe des utilisateurs (création, vérification, erreur)
-class User
+class User extends Controller
 {
 
-    public static function addUserInDatabase(mysqli $connect, string $username, string $password, string $email, string $token): bool
+    //Ajouter un utlisateur dans la base de données (REGISTER)
+    public static function addUserInDatabase(mysqli $connect, string $username, string $password, string $email, string $token): int
     {
-        //verifier si l'utilisateur n'est pas déja dans la BDD
-        //ajouter user
-        return false;
+        //Verifier si l'utilisateur n'est pas déja dans la BDD
+        if (self::checkUsername($connect, $username, $password)) //Username deja utilisé
+        {
+            return -1;
+        } else if  (self::checkEmail($connect, $email)) //Email deja utilisé
+        {
+            return -2;
+        }
+        //Ajouter l'utilisateur
+        self::insertData($connect, "users", "username=?,password=?,email=?,token=?", [$username, $password, $email, $token]);
+        return 1;
+
     }
 
 
-    //Vérifie si l'utilisateur existe dans la base de données
-    public static function isUserInDatabase(mysqli $connect, string $username, string $password, string $email): bool
+    //Vérifie si l'utilisateur existe dans la base de données (LOGIN)
+    public static function isUserInDatabase(mysqli $connect, string $username, string $password): int
     {
-        //verifier mdp
+        //On verifie que le mot de passe, le nom d'utilisateur entrés sont stockés dans la DBB
+        
+        if (!self::checkUsername($connect, $username)) //Nom utilisateur incorrect 
+        {
+            return -1;
+        } else if (!self::checkPassword($connect, $username, $password)) //Mot de passe incorrect
+        {
+            return -2;
+        }
 
-        //verifier email
-
-        //verifier username
-
-        return false;
+        return 1;
     }
 
 
+    static function passwordMatch(string $password, string $passwordConfirm)
+    {
+        # code...
+    }
 
     //Verifie si le mot de passe entré est le même que celui stocké dans la BDD	
-    function checkPassword(mysqli $connect, string $username, string $passwordToCheck): bool
+    static function checkPassword(mysqli $connect, string $username, string $passwordToCheck): bool
     {
-        if ($res = Controller::fetchData($connect, "password", "users", "WHERE username=?", [$username])) {
+        if ($res = self::fetchData($connect, "password", "users", "WHERE username=?", [$username])) {
             //On verifie qu'on a qu'un seul resultat rendu
             if (sizeof($res) == 1) {
                 if (password_verify($passwordToCheck, $res["password"])) {
                     return true;
                 }
             }
+        }
+        return false;
+    }
+
+    //Verifie si le nom d'utilisateur entré est déjà dans la BDD
+    static function checkUsername(mysqli $connect, string $username): bool
+    {
+        if (self::fetchData($connect, "*", "users", "WHERE username=?", [$username])) {
+            return true;
+        }
+        return false;
+    }
+
+    //Verifie si l'adresse mail entré est déjà dans la BDD
+    static function checkEmail(mysqli $connect, string $email): bool
+    {
+        if (self::fetchData($connect, "*", "users", "WHERE email=?", [$email])) {
+            return true;
         }
         return false;
     }
