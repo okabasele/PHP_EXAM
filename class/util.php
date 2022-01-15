cce6de069f62b56547d50ebf191092a4690cc2a6
okabasele <ocekabasele@gmail.com>
2022-01-15 11:01:43 +0100

    fonction redirect

--- class/util.php
+++ class/util.php
@@ -6,19 +6,25 @@ class Util
 {
     static ?DatabaseConnection $connection = null;
 
-    static function getDatabaseConnection() : DatabaseConnection
+    static function getDatabaseConnection(): DatabaseConnection
     {
         //S'il n'y a pas de connection à la base de données on en crée une
-        if(!self::$connection){
+        if (!self::$connection) {
             self::$connection = new DatabaseConnection('root', '', 'php_exam_db');
         }
         //Sinon on retourne la connection déjà existante (évite les connections/déconnection répétitive)
         return self::$connection;
     }
 
-    static function generateToken(int $length) : string
+    static function generateToken(int $length): string
     {
         $alphabet = "0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
         return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
+    }
+
+    static function redirect($url)
+    {
+        header('Location: ' . $url);
+        die();
     }
 }
