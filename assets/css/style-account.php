4d6598f7d59d6758ea5bbff4bc7ba8944c1a69f0
LZOSSOUNGBO <lorine.zossoungbo@ynov.com>
2022-01-22 23:55:11 +0100

    23h54

--- assets/css/style-account.php
+++ assets/css/style-account.php
@@ -24,6 +24,35 @@ body{
  margin-bottom: 1.5rem;
 box-shadow: 0 2px 6px 0 rgb(218 218 253 / 65%), 0 2px 6px 0 rgb(206 206 238 / 54%);
 }
+
+button{
+    display: inline-block;
+  padding: 10px 24px;
+  font-size: 10px;
+  font-family: Merriweather;
+  width:100px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: black;
+  background-color: white;
+  border: 1px solid #4CAF50;
+  border-radius: 8px;
+
+}
+
+button:hover {
+ background-color: #4CAF50; /* Green */
+  color: white;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+
+button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
 .me-2 {
     margin-right: .5rem!important;
 }
