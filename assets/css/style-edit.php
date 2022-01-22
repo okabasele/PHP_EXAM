9a5f59557c3f6224641b25c79a32286ffc68a291
LZOSSOUNGBO <lorine.zossoungbo@ynov.com>
2022-01-22 23:58:41 +0100

    23h58

---
+++ assets/css/style-edit.php
@@ -0,0 +1,60 @@
+<style>
+
+body{
+    background: #D4EFF2;
+    margin-top:20px;
+}
+.card {
+    position: absolute ;
+    display: flex;
+    flex-direction: column;
+    width: 70vw;
+    top: 10%;
+    left: 15%;
+    right: 20%;
+    word-wrap: break-word;
+    background-color: #FFFCF0;
+    background-clip: border-box;
+    border: 0 solid transparent;
+    display: flex;
+  justify-content: center;
+  flex-direction: row;
+  padding: 50px 10px;
+ border-radius: .25rem;
+ margin-bottom: 1.5rem;
+box-shadow: 0 2px 6px 0 rgb(218 218 253 / 65%), 0 2px 6px 0 rgb(206 206 238 / 54%);
+}
+
+button{
+    display: inline-block;
+  padding: 10px 24px;
+  font-size: 10px;
+  font-family: Merriweather;
+  width:100px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: black;
+  background-color: white;
+  border: 1px solid #4CAF50;
+  border-radius: 8px;
+
+}
+
+button:hover {
+ background-color: #4CAF50; /* Green */
+  color: white;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+
+button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+.me-2 {
+    margin-right: .5rem!important;
+}
+
+</style>
